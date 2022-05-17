2ed3dfaaa37c2f2e5b080c0f56780b0a55648aff
Tyler Martin <martint721@gmail.com>
2022-05-17 12:53:04 -0400

    Bugfix for ang.py and adding test script ang_loading_test.py

--- orix/io/plugins/ang.py
+++ orix/io/plugins/ang.py
@@ -353,10 +353,13 @@ def _get_phases_from_header(header):
 
     # If phases["point_group"] is a number, assume symmetry to be space group and convert to point group international
     # notation
-    my_symmetry = phases["point_group"][0]
-    if my_symmetry.isdigit():
-        space_group = int(my_symmetry)
-        phases["point_group"] = _get_space_group_from_point_group(space_group)
+    point_group_names = ['1', '-1', '2', 'm', '2/m', '222', 'mm2', 'mmm', '4', '-4', '4/m', '422', '4mm', '42m',
+                         '4/mmm', '3', '-3', '32', '3m', '-3m', '6', '-6', '6/m', '622', '6mm', '-6m2', '6/mmm', '23',
+                         'm-3', '432', '-43m', 'm-3m']
+    for i, phase in enumerate(phases["point_group"]):
+        if phase.isdigit() and phase not in point_group_names:
+            space_group = int(phase)
+            phases["point_group"][i] = _get_space_group_from_point_group(space_group)
 
     return phase_ids, names, phases["point_group"], phases["lattice_constants"]
 

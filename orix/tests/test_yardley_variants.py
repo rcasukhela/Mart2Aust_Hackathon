f321b6b496e501c2e4ecc87f2d22afe767ee81cc
Rohan Casukhela <43920866+rcasukhela@users.noreply.github.com>
2022-08-08 15:31:32 -0400

    In the middle of trying to get both MATLAB-to-Python Pythonic Yardley Variants to check out...
    Not exactly sure what's up, but both Eric's implementation (MATLAB to Python translation) and my implementation of Yardley Variants do not produce Bain correspondence matrices that match up with Kitahara's work for either case. Will have to raise an issue on this.

--- orix/tests/test_yardley_variants.py
+++ orix/tests/test_yardley_variants.py
@@ -21,15 +21,42 @@ import pytest
 from reconstruction.modules import yardley_variants
 
 import numpy as np
-from math import sin, cos
-from  scipy.spatial.transform import Rotation as R
+from math import radians
+from scipy.spatial.transform import Rotation as R
 
 
-def test_yardley_variants():
-    ksi = [5.5, 5.0, 10.5]
+def test_yardley_variants_nw():
+    '''
+    check_matrix is T1 from Table 3 of Kitahara 2004.
+    '''
+    ksi = [radians(5.26), radians(10.30), radians(10.5)]
 
     yv_test = yardley_variants(ksi)
     
-    extreme_values_matrix = None
+    check_matrix = [
+        [0.0, 0.707, -0.707],
+        [-0.169, 0.697, 0.697],
+        [0.986, 0.120, 0.120]
+    ]
+
+    print(f'{yv_test}, {check_matrix}')
+
+    assert np.array_equal(yv_test[0], check_matrix)
+
+
+def test_yardley_variants_ks():
+    '''
+    check_matrix is T1 from Table 3 of Kitahara 2006.
+    '''
+    ksi = [radians(0), radians(9.74), radians(9.74)]
+
+    yv_test = yardley_variants(ksi, rot='ZYZ')
     
-    assert yv_test[0] == extreme_values_matrix
+    check_matrix = [
+        [0.742, -0.667, -0.075],
+        [0.650, 0.742, -0.167],
+        [0.167, 0.075, 0.983]
+    ]
+    print(f'{yv_test}, {check_matrix}')
+    
+    assert np.array_equal(yv_test[0], check_matrix)
